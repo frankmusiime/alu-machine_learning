dbabd8f7a1ca958296523585959d4e76698553ea
frankmusiime <f.musiime@alustudent.com>
2026-05-17 09:44:42 -0700

    files

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Function that performs matrix multiplication using NumPy."""
 
-
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
+    """Returns the matrix product of two numpy.ndarrays."""
     return np.matmul(mat1, mat2)
